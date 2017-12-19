523ac2a74b4785608ca85f91d19c3be3c146d1c0
zkuhn <zakuhn@fusionent.com>
2017-12-19 23:07:06 +0000

    initial commit

---
+++ BikeKinematics.py
@@ -0,0 +1,11 @@
+from EstimatedPose import EstimatedPose
+class BikeKinematics :
+
+    def _init_():
+        """Create a bike model pointing at heading 0 centered at (0,0)"""
+        pass
+    
+    def get_pose():
+        pass
+
+   
---
+++ EstimatedPose.py
@@ -0,0 +1,6 @@
+class EstimatedPose :
+
+    def _init_(self,):
+        """Representation of an arbitrary item in 2d space."""
+      
+
