e4597fa22118a5d6992d04e834af0827cbfbea45
zkuhn <zakuhn@fusionent.com>
2017-12-20 09:46:10 +0000

    make a test case work with the ported code

--- EstimatedPose.py
+++ EstimatedPose.py
@@ -1,6 +1,11 @@
 class EstimatedPose :
 
-    def _init_(self,):
+    def __init__(self,x=0, y=0, heading=0):
         """Representation of an arbitrary item in 2d space."""
-      
+        self.x = x
+        self.y = y
+        self.heading = heading
+    
+    def __str__(self):
+        return "x: " + str(self.x) + " y: " + str(self.y) + " heading: " + str(self.heading) + "\n"
 
