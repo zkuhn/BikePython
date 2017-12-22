3aa53f2f42db5436ad67f54fff9b5a346da60c66
zkuhn <zakuhn@fusionent.com>
2017-12-22 16:49:40 +0000

    Correct the call order, confirm tests

--- test_BikeKinematics.py
+++ test_BikeKinematics.py
@@ -3,16 +3,14 @@ from EstimatedPose import EstimatedPose
 import copy
 import math
 
+EPSILON = .0001
+
 def test_BikeTurns():
     bike = get_default_bike()
-    new_pose = bike.estimate(12, 250, math.pi/4, 100)
-    new_pose = bike.estimate(12, 250, -math.pi/4, 100)
+    #turn left, then turn right the same amount. HSould be facing back to start
+    new_pose = bike.estimate(12, math.pi/4, 250, 100)
+    new_pose = bike.estimate(12, -math.pi/4, 250, 100)
     assert new_pose.heading == 0
-
-def test_BikeCircles():
-    bike = get_default_bike() 
-    new_pose = bike.estimate(12, 512*6, -math.pi/4, 100)
-    assert new_pose.heading <.00089
 
 def get_default_bike():
 
@@ -36,3 +34,25 @@ def test_turning_radius():
     assert bike.get_turning_radius(math.pi/2) == bike.hub_distance
     # a 30 degree turn should yield a radius twice the hub distance
     assert abs(bike.get_turning_radius(math.pi/6) -  bike.hub_distance * 2 ) < .004
+
+def test_half_circle():
+    
+
+    #Make a bike with front diameter of 1, so the wheel will go pi distance.
+    pose = EstimatedPose()
+    assert pose.heading ==0
+    bike = BikeKinematics(.5, 1.0, 512, pose)
+    rpose = bike.estimate(1, math.pi/6, 512, 0)
+
+    assert rpose.heading - math.pi/2 < EPSILON
+    assert rpose.x - 2 <EPSILON
+
+def test_full_circle_left():
+
+ 
+    pose = EstimatedPose()
+    assert pose.heading ==0
+    bike = BikeKinematics(.5, 1.0, 512*2, pose)
+    rpose = bike.estimate(1, math.pi/6, 512, 0)
+
+    assert rpose.heading - math.pi/2 < EPSILON
