5453df37dc4359937338807f307676d1fc679224
zkuhn <zakuhn@fusionent.com>
2017-12-22 18:46:37 +0000

    add some heading normalization tests

--- test_BikeKinematics.py
+++ test_BikeKinematics.py
@@ -5,6 +5,9 @@ import math
 
 EPSILON = .0001
 
+def almost_zero(value):
+    return abs(value) < EPSILON
+
 def test_BikeTurns():
     bike = get_default_bike()
     #turn left, then turn right the same amount. HSould be facing back to start
@@ -41,18 +44,44 @@ def test_half_circle():
     #Make a bike with front diameter of 1, so the wheel will go pi distance.
     pose = EstimatedPose()
     assert pose.heading ==0
-    bike = BikeKinematics(.5, 1.0, 512, pose)
-    rpose = bike.estimate(1, math.pi/6, 512, 0)
+    bike = BikeKinematics(.5, 1.0, 512 , pose)
 
-    assert rpose.heading - math.pi/2 < EPSILON
-    assert rpose.x - 2 <EPSILON
+    #confirm the turning radius is 2
+    assert almost_zero( bike.get_turning_radius(math.pi/6) - 2)
+    #onfirm if we go a revolution of the wheel, we wil go pi radians
+    assert almost_zero( bike.get_radians_travelled(math.pi/6 , 2*math.pi) - math.pi)
+    
+    rpose = bike.estimate(1, math.pi/6, 512 *2, 0)
+
+    assert almost_zero( rpose.heading - math.pi )
+    assert almost_zero( rpose.x )
+
+    #the front wheel goes 4 units up, the back wheel takes a slightly shorter path
+    assert  rpose.y  < 4
 
 def test_full_circle_left():
-
  
     pose = EstimatedPose()
     assert pose.heading ==0
-    bike = BikeKinematics(.5, 1.0, 512*2, pose)
-    rpose = bike.estimate(1, math.pi/6, 512, 0)
+    bike = BikeKinematics(.5, 1.0, 512, pose)
+    rpose = bike.estimate(1, math.pi/6, 512*4, 0)
 
-    assert rpose.heading - math.pi/2 < EPSILON
+    assert rpose.heading  < EPSILON
+    assert almost_zero(rpose.x)
+    assert almost_zero(rpose.y)
+
+def test_full_circle_right():
+ 
+    pose = EstimatedPose()
+    bike = BikeKinematics(.5, 1.0, 512, pose)
+    rpose = bike.estimate(1, -math.pi/6, 512*4, 0)
+
+    assert rpose.heading  < EPSILON
+    assert almost_zero(rpose.x)
+    assert almost_zero(rpose.y)
+
+def test_heading_normalization():
+    bike = get_default_bike()
+    assert almost_zero(bike.normalize_heading(math.pi  + 1 ) + math.pi - 1)
+    assert almost_zero(bike.normalize_heading(3/2 * math.pi   ) + math.pi /2 ) 
+    assert almost_zero(bike.normalize_heading(3 * math.pi ) - math.pi )
